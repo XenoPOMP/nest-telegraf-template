a71c2bbbe979629bea4413fa289f00d827a7f103
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-02-03 19:29:17 +0300

    setup telegram bot

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TelegrafModule } from 'nestjs-telegraf';
+
+import * as LocalSession from 'telegraf-session-local';
+import * as process from 'process';
+
+const sessions = new LocalSession({ database: 'session_db.json' });
 
 @Module({
-	imports: [ConfigModule.forRoot()],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		TelegrafModule.forRoot({
+			middlewares: [sessions.middleware()],
+			token: process.env.TG_TOKEN,
+		}),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
